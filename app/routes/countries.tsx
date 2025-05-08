f5e173fe97cfc5abb6724d9979ec557f3eaa4f24
Xiaode Zheng <your.email@example.com>
2025-05-09 01:10:34 +0800

    country list

--- app/routes/countries.tsx
+++ app/routes/countries.tsx
@@ -1,7 +1,54 @@
-export default function Countries() {
+import { Link } from "react-router";
+import type { Route } from "../+types/root";
+import { v4 as uuid } from "uuid";
+import { useState } from "react";
+
+export async function clientLoader() {
+  const res = await fetch("https://restcountries.com/v3.1/all");
+  const data = await res.json();
+  return data;
+}
+
+interface ICountryData {
+  name: {
+    common: string;
+  };
+  region: string;
+  population: number;
+}
+
+
+export default function Countries({ loaderData = [] }: { loaderData?: ICountryData[] }) {
+  const [search, setSearch] = useState<string>("");
+  const filteredCountries = loaderData.filter((country: ICountryData) => {
+    const matchesSearch = !search || country.name.common.toLowerCase().includes(search.toLowerCase());
+    return matchesSearch;
+  })
+
   return (
     <div>
-      <h1>Countries</h1>
+      <h2> Countries</h2>
+
+      <div>
+        <input
+          type="text"
+          placeholder="Search by name..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
+      <ul>
+        {
+          filteredCountries.map((country: ICountryData) => (
+            <li key={uuid()}>
+              <Link to={`/countries/${country.name.common}`}> {country.name.common} </Link>
+              <div>
+                Region: {country.region} | Population: {country.population}
+              </div>
+            </li>
+          ))
+        }
+      </ul>
     </div>
   );
 }
